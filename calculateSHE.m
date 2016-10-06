a6821b3c8abb53f56a0505119dcaae753c9224be
Joakim Thorén <joakimthoren93@gmail.com>
2016-10-06 22:36:02 +0200

    fixed bug where SHE darkened image wherever it wasnt enhanced aswell as tuning range of sliders in GUI

--- calculateSHE.m
+++ calculateSHE.m
@@ -1,23 +1,14 @@
-%
-function dummy = calculateSHE(E, k0, k1, k2, surroundingSize, fig, forestgray, padoption)
-
 %Statistical histogram equalization
+function dummy = calculateSHE(E, k0, k1, k2, surroundingSize, fig, forestgray, padoption)
 
 %anonymous function for mean caluclation, possibly replace by matlab native
 imgMean = @(img) sum(sum(img))/(size(img,1)*size(img,2));
 %anonymous function for standard deviation caluclation, possibly replace by matlab native
 imgVar = @(img) sum(sum((img - imgMean(img)).^2))/(size(img,1)*size(img,2));
 
-
-%load('forest.mat')
-forestgray = double(forestgray);
-
 globalMean = imgMean(forestgray);
 globalVar = imgVar(forestgray);
-
 [M, N] = size(forestgray);
-
-%surroundingSize = 3;
 padSize = floor(surroundingSize/2);
 
 paddedImage = padarray2d(forestgray, padSize, padoption);
@@ -28,16 +19,8 @@ ys = 1 + padSize;
 yf = N + padSize;
 
 conditionalMatrix = forestgray * 0 + 1;
-conditionalMatrixMean = forestgray * 0 + 1; %Each cell should equal E where mean condition is satisfied
-conditionalMatrixVar = forestgray * 0 + 1; %Each cell should equal E where variance condition is satisfied
-
-debugMatrix = conditionalMatrix * 0;
-
-%conditions
-%E = 8.0;
-%k0 = 10000;%0.4;
-%k1 = 0;%0.02;
-%k2 = 0.4;
+conditionalMatrixMean = forestgray * 0;
+conditionalMatrixVar = forestgray * 0;
 
 for x = xs:xf
    for y = ys:yf 
@@ -49,18 +32,22 @@ for x = xs:xf
        varcond = (k1*globalVar <=surroundingVar) && (k2*globalVar >=surroundingVar);
        if (meancond && varcond)
            conditionalMatrix(x-padSize, y-padSize) = E;
-           debugMatrix(x-padSize, y-padSize) = 1;
        end
        if (meancond)
-           conditionalMatrixMean(x-padSize, y-padSize) = E;
+           conditionalMatrixMean(x-padSize, y-padSize) = 1;
        end
        if (varcond)
-           conditionalMatrixVar(x-padSize, y-padSize) = E;
+           conditionalMatrixVar(x-padSize, y-padSize) = 1;
        end
    end
 end
 
+%Ensure that amplification of eligible pixels don't exceed max value of
+%original image, such that other parts become darkened by matlab
 finalImage = forestgray .* conditionalMatrix;
+maxGray = max(max(forestgray))
+finalImage(finalImage >= maxGray) = maxGray;
+
 
 hold on;
 subplot(2,2,1)
@@ -79,9 +66,5 @@ subplot(2,2,4)
 imshow(conditionalMatrixVar, [])
 title('Only considering variance condition')
 hold off;
-
-
-%subplot(2,2,3)
-%imshow(debugMatrix, [])
 
 dummy = 3.14;
--- shePlayGui.m
+++ shePlayGui.m
@@ -9,25 +9,29 @@
     labFilterSize = uicontrol('Style', 'text', 'String','Filter size, odd numbers in range [1 50]', 'Position', [180*1-30 40 200 20]);
     sldFilterSize = uicontrol('Style', 'slider', 'Min',1,'Max',25,'Value',4,'Position', [180*1 20 140 20], 'Callback', @sheCallback);
     
-    labE = uicontrol('Style', 'text', 'String','Multiplicative value E [0 - 50]', 'Position', [180*2 40 140 20]);
-    sldE = uicontrol('Style', 'slider', 'Min',0,'Max',50,'Value',4,'Position', [180*2 20 120 20], 'Callback', @sheCallback); 
+    labE = uicontrol('Style', 'text', 'String','Multiplicative value E [0 - 20]', 'Position', [180*2 40 140 20]);
+    sldE = uicontrol('Style', 'slider', 'Min',1,'Max',20,'Value',4,'Position', [180*2 20 120 20], 'Callback', @sheCallback); 
 	
     labk0 = uicontrol('Style', 'text', 'String', 'Mean coeff. k0 [0 - 10]', 'Position', [180*3 - 15 40 150 20]);
     sldk0 = uicontrol('Style', 'slider', 'Min',0,'Max',10,'Value',0.4, 'Position', [180*3 20 120 20], 'Callback', @sheCallback); 
     
-    labk1 = uicontrol('Style', 'text', 'String', 'Lower limit local standard dev. k1 (1 - 10)', 'Position', [180*4 - 60 40 240 20]);
-    sldk1 = uicontrol('Style', 'slider', 'Min',0,'Max',10,'Value',0.02, 'Position', [180*4 20 120 20], 'Callback', @sheCallback);     
+    labk1 = uicontrol('Style', 'text', 'String', 'Lower limit local standard dev. k1 (0 - 1)', 'Position', [180*4 - 60 40 240 20]);
+    sldk1 = uicontrol('Style', 'slider', 'Min',0,'Max',1,'Value',0.02, 'Position', [180*4 20 120 20], 'Callback', @sheCallback);     
     
-    labk2 = uicontrol('Style', 'text', 'String', 'Standard dev. coeff. k2 (1 - 10)', 'Position', [180*5 40 160 20]);
-    sldk2 = uicontrol('Style', 'slider', 'Min',0,'Max',10,'Value',0.4, 'Position', [180*5 20 120 20], 'Callback', @sheCallback);
+    labk2 = uicontrol('Style', 'text', 'String', 'Standard dev. coeff. k2 (0 - 1)', 'Position', [180*5 40 160 20]);
+    sldk2 = uicontrol('Style', 'slider', 'Min',0,'Max',1,'Value',0.4, 'Position', [180*5 20 120 20], 'Callback', @sheCallback);
     
     f.Visible = 'on';
 
     loadedForest = load('forest');
     rawFilterSize = sldFilterSize.Value;
     correctFilterSize = 2*floor(sldFilterSize.Value)-1;
-    forestgray = loadedForest.forestgray;
-    calculateSHE( sldE.Value, sldk0.Value, sldk1.Value, sldk2.Value, 3, f, forestgray, 'zero'); 
+    forestgray = mat2gray(loadedForest.forestgray);
+    
+    allItems = get(padoptionDropdown,'string');
+    selectedIndex = get(padoptionDropdown,'Value');
+    selectedItem = allItems{selectedIndex};
+    calculateSHE( sldE.Value, sldk0.Value, sldk1.Value, sldk2.Value, correctFilterSize, f, forestgray, selectedItem); 
 
     function sheCallback(source,event)
         rawFilterSize = sldFilterSize.Value;
